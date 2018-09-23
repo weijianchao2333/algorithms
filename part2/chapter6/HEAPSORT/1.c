b25ed7602c9261b7acfce1ab24d49187635f9564
weijianchao <weijianchao2333@gmail.com>
2018-09-23 15:09:42 +0800

    update to 6.4

---
+++ part2/chapter6/HEAPSORT/1.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+int Left(int i)
+{
+	return i << 1;
+}
+
+int Right(int i)
+{
+	return (i << 1) + 1;
+}
+
+void Max_heapify(int * a, int i, int size)
+{
+	int l = Left(i);
+	int r = Right(i);
+	int largest;
+	if(l <= size && a[l] > a[i])
+		largest = l;
+	else largest = i;
+	if(r <= size && a[r] > a[largest])
+		largest = r;
+	if(largest != i)
+	{
+		a[i] ^= a[largest];
+		a[largest] ^= a[i];
+		a[i] ^= a[largest];
+		Max_heapify(a,largest,size);
+	}
+
+}
+
+void Build_max_heap(int * a, int size)
+{
+	int i;
+	for(i = size/2; i >= 1; i--)
+		Max_heapify(a,i,size);
+}
+
+void Heapsort(int * a, int size)
+{
+	Build_max_heap(a,size);
+	int i;
+	for(i = size; i >= 2; i--)
+	{
+		a[i] ^= a[1];
+		a[1] ^= a[i];
+		a[i] ^= a[1];
+		size--;
+		Max_heapify(a,1,size);
+	}
+}
+
+
+
+void Show(int * a, int size)
+{
+	int i;
+	for(i = 1; i <= size; i++)
+		printf("%d ", a[i]);
+	printf("\n");
+}
+
+int main()
+{
+	int size = 10;
+	int a[] = {0,4,1,3,2,16,9,10,14,8,7};//从下标为1处开始
+	Heapsort(a,size);
+	Show(a,size);
+	return 0;
+}
